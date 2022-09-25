31a8a88c1c4708ff54ddf3a975bcdf4643b5685e
lucas-perata <contacto.perata@gmail.com>
2022-09-25 16:59:39 -0300

    Fixed modelos footer y transaction

--- app/models/footer.rb
+++ app/models/footer.rb
@@ -3,5 +3,6 @@
 class Footer < ApplicationRecord
   belongs_to :client
   has_one :header
+  attribute :reserved, :string, default: '               '
   validates_presence_of :client_id
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,6 @@
 class Transaction < ApplicationRecord
   belongs_to :client
   attribute :reserved, :string, default: '     '
+  validates_presence_of :client_id
+  validates_presence_of :transaction_id
 end
