e4638994d9c48b2fc0287d2b4edd755b6e80794f
lucas-perata <contacto.perata@gmail.com>
2022-09-25 17:01:40 -0300

    Filter controlador: respuesta a errores de la request a la API

--- app/controllers/api/v1/filter_controller.rb
+++ app/controllers/api/v1/filter_controller.rb
@@ -3,38 +3,62 @@
 module Api
   module V1
     class FilterController < ApplicationController
-      require 'faker'
-      def index
+
+      @@count = 0 
+
+      def load
         response = HTTParty.get('https://increase-transactions.herokuapp.com/file.txt',
                                 headers: { 'Authorization' => ENV['API_KEY'] })
+        if response.code == 200 
+          filter_request(response)
+        elsif response.code == 500 && @@count < 2 
+          sleep 2 
+          puts "Trying again"
+          @@count += 1
+          load   
+        elsif response.code == 500 && @@count == 3 
+          render json: { message: response.code }
+        end 
+      
+      end
+
+      def filter_request(response)
         response_body = response.body
-        response_string = response_body
-        r = response_string.lines
+        r = response_body.lines
         n = r.count
         b = 0
 
-        while n > b
+        while n > b && @@count < 6 do 
           # Clients filter
           filter_line = r[n - 1]
 
           if r[n - 1][0].to_i === 4
-            new_client(filter_line[24..55])
+            get_client(filter_line[24..55])
+            #Client.create(client_id: filter_line[24..55])
             new_footer(filter_line[16..23])
           # Header Filter
           elsif r[n - 1][0].to_i === 2
             new_transaction(filter_line[1..32], filter_line[33..46], filter_line[51])
           elsif r[n - 1][0].to_i === 1
-            new_header(filter_line[1..32], filter_line[35..38], filter_line[39..51], filter_line[52..64],
-                       filter_line[65..78])
+           new_header(filter_line[1..32], filter_line[35..38], filter_line[39..51], filter_line[52..64], filter_line[65..78])
           else
-            puts 'Table'
+            #puts 'Table'
           end
           n -= 1
         end
-      end
+      end 
 
-      def new_client(id)
-        Client.create(client_id: id, first_name: Faker::Name.name, last_name: Faker::Name.name)
+      def new_client(response)
+        id = response["id"] 
+        email = response["email"]
+        first_name = response["first_name"]
+        last_name = response["last_name"]
+        job = response["job"]
+        address = response["address"]
+        zip_code = response["zip_code"]
+        phone = response["phone"]
+
+        Client.create(client_id: id, first_name: first_name, last_name: last_name, email: email, job: job, address: address, zip_code: zip_code, phone: phone)
       end
 
       def new_transaction(id, amount, type)
@@ -43,12 +67,43 @@ module Api
 
       def new_header(id, currency, total, discounts, total_with_discounts)
         Header.create(client_id: Client.last.id, payment_id: id, currency: currency, total: total,
-                      discounts: discounts, total_with_discounts: total_with_discounts)
+                      discounts: discounts, total_with_discounts: total_with_discounts, footer_id: Footer.last.id)
       end
 
       def new_footer(date)
-        Footer.create(client_id: Client.last.id, payment_date: date)
+        Footer.create(client_id: Client.last.id, payment_date: date) unless Client.last.nil?
       end
+
+      def get_client(id)
+        client_id = id
+        response = HTTParty.get('https://increase-transactions.herokuapp.com/clients/' + client_id,
+          headers: { 'Authorization' => ENV['API_KEY'] })
+
+            if response.code == 200
+              puts id
+              sleep 1
+              response_body = response.body
+              parsed_response = JSON.parse(response_body)
+              puts parsed_response
+              new_client(parsed_response)
+              return 
+            elsif response.code == 500 && @@count < 6
+              puts "Error server: waiting to do the request again" 
+              @@count = @@count + 1 
+              puts @@count  
+              sleep 4
+              puts "Trying again"
+              get_client(client_id)
+            elsif response.code == 500 && @@count == 6
+              puts "hola"
+              puts @@count 
+              sleep 2 
+              return
+          end 
+      end 
+
+     
+
     end
   end
 end
