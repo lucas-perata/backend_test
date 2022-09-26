873166c3fc66113dcdbefafc8c027cff755a3568
lucas-perata <contacto.perata@gmail.com>
2022-09-26 16:21:02 -0300

    Respuesta a ACTIVERECORD::NOTFOUND

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render json: {error: "not-found"}.to_json, status: 404
+  end
 end
